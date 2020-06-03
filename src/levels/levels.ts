a3d4d0960a9b6abd2fcd66fd1a72708448ef664b
Yosef Tukachinsky <yoostuk@gmail.com>
2020-06-03 23:59:43 +0300

    fix the levels order

--- src/levels/levels.ts
+++ src/levels/levels.ts
@@ -3,6 +3,6 @@ import Level1 from "./level1";
 import Level2 from "./level2";
 import Level3 from "./level3";
 
-const LEVELS: Array<() => Block[]> = [Level3, Level2, Level3];
+const LEVELS: Array<() => Block[]> = [Level1, Level2, Level3];
 
 export default LEVELS;
